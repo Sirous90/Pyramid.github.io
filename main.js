bfed4d675d7a6b276f7b29471f08376406aff704
SIROUS <sirousbenaicha@hotmail.fr>
2020-09-08 16:03:30 +0100

    a

--- main.js
+++ main.js
@@ -2,17 +2,19 @@ var readline = require('readline');
 var rl = readline.createInterface( process.stdin , process.stdout );
 
 rl.question(" Salut,bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?\n>",function pyramide(e){
-if (e>25) {
-	return;
+	e=Math.round(e);
+	console.log(e);
+if (e>25||e<=1)  {
+	return 0;
 }
 else
 {
 	console.log(" Voici la pyramide:");
 
-	const Pm = Math.floor(((e*2)-1)/2);
+	const Pm =Math.round(((e*2)-1)/2);
 	for (i = 0; i<e; i++) {
 		var niveau = ' ';
-	for (j = 0; j<(e*2)-1; j++) {
+	for (j = 0; j<=(e*2)-1; j++) {
 		if (Pm - i <= j && Pm + i>= j) {
 			niveau+='#';
 		}
